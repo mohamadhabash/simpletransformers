0fe3fb5f1fa110df83606f2638976cf47b48efee
Mohammad Habash <mhabash17@gmail.com>
2022-01-13 19:14:00 +0200

    Error '1 is not in list' when using Multimodal with argument multi_label=True

--- simpletransformers/classification/classification_utils.py
+++ simpletransformers/classification/classification_utils.py
@@ -833,7 +833,7 @@ class JsonlDataset(Dataset):
         if self.multi_label:
             label = torch.zeros(self.n_classes)
             label[
-                [self.labels.index(tgt) for tgt in self.data[index][self.labels_label]]
+                [self.labels.index(str(tgt)) for tgt in self.data[index][self.labels_label]]
             ] = 1
         else:
             label = torch.tensor(self.labels.index(self.data[index][self.labels_label]))
